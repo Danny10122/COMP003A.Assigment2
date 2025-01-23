62da3fe022bf54695f2da379a6b53d0bd35d2561
Danny10122 <danieljuarez5575@my.whccd.edu>
2025-01-22 20:42:25 -0800

    code structure

--- COMP003A.Assigment2/Program.cs
+++ COMP003A.Assigment2/Program.cs
@@ -1,4 +1,10 @@
-﻿namespace COMP003A.Assigment2
+﻿ /*
+    Author: Daniel Juarez
+    Course: COMP-003A
+    Faculty: Danel juarez
+    Purpose: Discount calculator
+ */
+namespace COMP003A.Assigment2
 {
     internal class Program
     {
@@ -20,7 +26,7 @@
             isStudent = bool.Parse(Console.ReadLine());
             int futureAge = userAge + 5; // Calculate the user's age in 5 years
                 string username = Console.ReadLine(); // gets the user input and it to
-            Console.WriteLine($"\n\n\nHello," + userName + "!"); // Great the user with their name
+                Console.WriteLine($"\n\n\nHello," + userName + "!"); // Great the user with their name
             Console.WriteLine($"You are curremtly {userAge} years old. \nIn 5 years, you will be {futureAge}."); // Display current and future age 
             Console.WriteLine($"The original price of the item is ${productPrice}."); // Display the price 
             Console.WriteLine($"As a student, your discounted price is ${productPrice * .9}."); // Display the price 
